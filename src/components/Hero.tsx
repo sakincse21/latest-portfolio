56a4829f9f2d4f2da9ab261148eb2b687a2711dd
sakincse21 <saleheen.sakin@gmail.com>
2025-09-01 17:49:31 +0600

    resume update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
           </div>
 
           <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
-            <a
+             <a
               href="https://drive.google.com/file/d/1VmYR8kHNNAuXTYH10-fUUkQC0A4H9EAM/view?usp=sharing"
               target="_blank"
             >
